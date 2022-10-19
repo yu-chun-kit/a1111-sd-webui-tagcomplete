10a8f071af15d7139b1beeb9bade91b5a7a72bb8
yu-chun-kit <johnson37siumen@hotmail.com>
2022-10-19 23:24:51 +0800

    change to use nga@猫舌头的阿巧 source for translation

--- load_tag_translation_CN.py
+++ load_tag_translation_CN.py
@@ -1,5 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
 import csv
 from os import linesep
 
@@ -11,33 +9,29 @@ def read_csv(path):
 
 
 tag_csv = "./tags/danbooru.csv"
-tag_csv_cn = "./tags/danbooru_cn.csv"
-
-
-url = 'https://wolfchen.top/tag/?from=zhihu'
-headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+tag_csv_zh = "./tags/danbooru-0-zh.csv"
+tag_csv_new = "./tags/danbooru-zh.csv"
 
 
 if __name__ == "__main__":
-    r = requests.get(url.strip(), headers=headers, timeout=10)
-
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    tags = soup.select(".layui-form > input[name=tag]")
-    tags = [x['title'].strip('\r\n') for x in tags]
 
     csv_list = read_csv(tag_csv)
-    csv_size = len(csv_list)
+    csv_list_zh = read_csv(tag_csv_zh)
+    print(len(csv_list))
+    print(len(csv_list_zh))
 
-    for i in range(len(csv_list)):
-        if len(csv_list[i]) == 3:
+    for i in range(len(csv_list_zh)):
+        if len(csv_list_zh[i]) != 3:
+            print("error {}".format(csv_list_zh[i]))
             continue
-        for t in tags:
-            if t.startswith(csv_list[i][0]):
-                csv_list[i].append(t.replace(csv_list[i][0], ''))
+        for j in range(len(csv_list)):
+            if csv_list[j][0] != "0" or len(csv_list[j]) == 3:
+                continue
+            if csv_list_zh[i][0] == csv_list[j][0]:
+                csv_list[j].append(csv_list_zh[i][2])
                 break
 
 
-    with open(tag_csv_cn, "w", encoding="UTF8", newline=None) as file:
+    with open(tag_csv_new, "w", encoding="UTF8", newline=None) as file:
         writer = csv.writer(file, lineterminator="\n")
         writer.writerows(csv_list)
